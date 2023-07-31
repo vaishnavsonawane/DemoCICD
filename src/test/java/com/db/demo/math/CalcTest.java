b801949ed4b61ad60d2780b47b200371fa9a9c33
VaishnavSonawane <vaishnav.sonawane19@vit.edu>
2023-07-31 16:29:32 +0530

    new test cases

---
+++ src/test/java/com/db/demo/math/CalcTest.java
@@ -0,0 +1,41 @@
+package com.db.demo.math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class CalcTest {
+
+    Calc calc = new Calc();
+
+    // positive test case
+    @Test
+    public void testAddNums() {
+        assertEquals(15, calc.addNums(10, 5));
+    }
+
+    // another positive test case
+    @Test
+    public void testAddNums2() {
+        assertEquals(20, calc.addNums(10, 10));
+    }
+
+    // negative test case
+    @Test
+    public void testAddNums3() {
+        assertNotEquals(20, calc.addNums(10, 5));
+    }
+
+    // another negative test case
+    @Test
+    public void testAddNums4() {
+        assertNotEquals(10, calc.addNums(10, 5));
+    }
+
+    // another negative test case
+//    @Test
+//    public void testAddNums5() {
+//        assertNotEquals(15, calc.addNums(10, 5));
+//    }
+}
